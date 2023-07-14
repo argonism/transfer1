09514a03e253479c010c354d78f48de05d71584d
kush <kouta528bado@yahoo.co.jp>
2023-07-15 00:44:48 +0900

    Refactor eval.py

--- src/eval.py
+++ src/eval.py
@@ -14,11 +14,11 @@ os.environ["INDEX"] = "../indexes/ntcir17-transfer/jance"
 os.environ["RUN"] = "../runs/ntcir17-transfer/jance"
 
 import ir_datasets
-import models
 import ntcir_transfer
 import pandas as pd
 import pyterrier as pt
 from models.jance.jance import PyTDenseIndexer, PyTDenseRetrieval
+from pyterrier.datasets import Dataset
 from pyterrier.measures import nDCG
 from sudachipy import dictionary, tokenizer
 
@@ -29,64 +29,90 @@ if not pt.started():
     pt.init(tqdm="notebook")
 
 
-def tokenize_text(text, tokenizer_obj, mode):
-    atok = " ".join([m.surface() for m in tokenizer_obj.tokenize(text, mode)])
-    return atok
+class Evaluator(object):
+    def __init__(
+        self,
+        dataset: Dataset,
+        sudachi_tokenizer: tokenizer.Tokenizer,
+        index_path: Path,
+        run_path: Path,
+        run_name: str = "kasys",
+        tokenizer_mode: str = tokenizer.Tokenizer.SplitMode.A,
+    ):
+        self.dataset = dataset
+        self.sudachi_tokenizer = sudachi_tokenizer
+        self.tokenizer_mode = tokenizer_mode
+        self.index_path = index_path
+        self.run_path = run_path
+        self.run_name = run_name
 
-
-def tokenize_topics(dataset, tokenizer_obj, mode):
-    import re
-
-    code = re.compile(
-        "[!\"#$%&'\\\\()*+,-./:;<=>?@[\\]^_`{|}~「」〔〕“”〈〉『』【】＆＊・（）＄＃＠。、？！｀＋￥％]"
-    )
-    queries = dataset.get_topics(tokenise_query=False)
-    for idx, row in queries.iterrows():
-        queries.iloc[idx, 1] = code.sub(
-            "", tokenize_text(row.query, tokenizer_obj, mode)
+    def tokenize_text(self, text):
+        atok = " ".join(
+            [
+                m.surface()
+                for m in self.sudachi_tokenizer.tokenize(text, self.tokenizer_mode)
+            ]
         )
-    return queries
+        return atok
+
+    def tokenize_topics(self, dataset):
+        code = re.compile(
+            "[!\"#$%&'\\\\()*+,-./:;<=>?@[\\]^_`{|}~「」〔〕“”〈〉『』【】＆＊・（）＄＃＠。、？！｀＋￥％]"
+        )
+        queries = dataset.get_topics(tokenise_query=False)
+        for idx, row in queries.iterrows():
+            queries.iloc[idx, 1] = code.sub("", self.tokenize_text(row.query))
+        return queries
+
+    def train_doc_generate(self):
+        for i, doc in enumerate(self.dataset.get_corpus_iter(verbose=False)):
+            doc["text"] = self.tokenize_text(doc["text"])
+            yield doc
+
+    def index(self):
+        indexer = pt.IterDictIndexer(str(self.index_path))
+        indexer.setProperty("tokeniser", "UTFTokeniser")
+        indexer.setProperty("termpipelines", "")
+        if not self.index_path.joinpath("shards.pkl").exists():
+            jance_indexer = PyTDenseIndexer(self.index_path, verbose=False)
+            index_path = jance_indexer.index(self.train_doc_generate())
+            return index_path
+        return str(self.index_path)
+
+    def eval_on_dev(self):
+        index_path = self.index()
+        index_path = Path(index_path)
+        if not index_path.exists():
+            index_path.mkdir(parents=True)
+
+        anceretr = PyTDenseRetrieval(index_path)
+
+        return pt.Experiment(
+            [anceretr],
+            self.tokenize_topics(self.dataset),
+            self.dataset.get_qrels(),
+            eval_metrics=[nDCG],
+            names=[self.run_name],
+            save_dir=self.run_path,
+            save_mode="overwrite",
+        )
 
 
-def train_doc_generate(dataset, tokenizer_obj, mode):
-    for i, doc in enumerate(dataset.get_corpus_iter(verbose=False)):
-        doc["text"] = tokenize_text(doc["text"], tokenizer_obj, mode)
-        yield doc
-
-
-def eval_on_dev():
+def main():
     dataset_pt = pt.get_dataset("irds:ntcir-transfer/1/train")
-    # for doc in dataset_pt.get_corpus_iter(verbose=False):
-    #     print(doc)
-    #     return
-    tokenizer_obj = dictionary.Dictionary().create()
+    sudachi_tokenizer = dictionary.Dictionary().create()
     mode = tokenizer.Tokenizer.SplitMode.A
 
-    indexer = pt.IterDictIndexer(os.getenv("INDEX"))
-    indexer.setProperty("tokeniser", "UTFTokeniser")
-    indexer.setProperty("termpipelines", "")
-    index_path = Path("../indexes/ntcir17-transfer/train/jance")
-    if not index_path.joinpath("shards.pkl").exists():
-        jance_indexer = PyTDenseIndexer(index_path, verbose=False)
-        index_path = jance_indexer.index(
-            train_doc_generate(dataset_pt, tokenizer_obj, mode)
-        )
-
-    if not Path(os.getenv("INDEX")).exists():
-        Path(os.getenv("INDEX")).mkdir(parents=True)
-
-    anceretr = PyTDenseRetrieval(index_path)
-    a = pt.Experiment(
-        [anceretr],
-        tokenize_topics(dataset_pt, tokenizer_obj, mode),
-        dataset_pt.get_qrels(),
-        eval_metrics=[nDCG],
-        names=["JANCE"],
-        save_dir=os.getenv("RUN"),
-        save_mode="overwrite",
+    evaluator = Evaluator(
+        dataset_pt,
+        sudachi_tokenizer,
+        Path("../indexes/ntcir17-transfer/debug/jance"),
+        os.getenv("RUN"),
+        tokenizer_mode=mode,
     )
-    print(a)
+    eval_result = evaluator.eval_on_dev()
+    print(eval_result)
 
 
 if __name__ == "__main__":
-    eval_on_dev()
+    main()
